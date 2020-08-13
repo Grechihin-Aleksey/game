203920ffd02e18ebd5c51830b6470361a734a259
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-13 23:26:17 +0300

    Ошибка в строке 38

--- script/script.js
+++ script/script.js
@@ -4,37 +4,36 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let startGame = function (numberForty) {
+let startGame = function () {
+  let numberForty = Math.floor(Math.random() * 101);
+  console.log("Случайное число " + numberForty);
+  let getData = function (flag) {
+    if (flag) {
+      getNumber();
+    } else {
+      alert("До скорой встречи!");
+    }
+  };
   let getNumber = function () {
-    let getData = function (flag) {
-      if (flag) {
-        getNumber();
-      } else {
-        alert("До скорой встречи!");
-      }
-    };
-
     let oneNumber = prompt("Угадай число от 1 до 100");
     let claim;
 
     if (!isNumber(oneNumber) || parseFloat(oneNumber) > 100) {
       claim = confirm("Введи число от 0 до 100!");
       getData(claim);
-
     } else if (oneNumber == numberForty) {
       alert("Правильно!");
-
     } else if (oneNumber > numberForty) {
       claim = confirm("Загаданное число меньше.");
       getData(claim);
-
     } else if (oneNumber < numberForty) {
       claim = confirm("Загаданное число больше.");
       getData(claim);
     }
+
+    return getNumber;
   };
-  return getNumber;
 };
 
-let number = startGame(40);
+let number = startGame();
 number();
