345223f4d7597e1087794c34594428a1161b14d3
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-14 20:43:25 +0300

    доп задание

--- script/script.js
+++ script/script.js
@@ -1,38 +1,82 @@
-"use strict";
+'use strict'
 
 let isNumber = function (n) {
+
   return !isNaN(parseFloat(n)) && isFinite(n);
+
 };
 
-let startGame = function () {
+let createGame = function () {
+
   let numberForty = Math.floor(Math.random() * 101);
-  console.log("Случайное число " + numberForty);
-  let getData = function (flag) {
+  let setValue = 10;
+  console.log("Неизвестное число", numberForty);
+
+  let getClaim = function (flag) {
+
     if (flag) {
       getNumber();
     } else {
-      alert("До скорой встречи!");
+      alert('Спасибо, что поиграли со мной');
     }
   };
-  let getNumber = function () {
-    let oneNumber = prompt("Угадай число от 1 до 100");
-    let claim;
 
-    if (!isNumber(oneNumber) || parseFloat(oneNumber) > 100) {
-      claim = confirm("Введи число от 0 до 100!");
-      getData(claim);
-    } else if (oneNumber == numberForty) {
-      alert("Правильно!");
-    } else if (oneNumber > numberForty) {
-      claim = confirm("Загаданное число меньше.");
-      getData(claim);
-    } else if (oneNumber < numberForty) {
-      claim = confirm("Загаданное число больше.");
-      getData(claim);
+  let counterTry = function (userTry) {
+    userTry--;
+    return userTry
+  };
+
+  let incorrectAnswer = function (claim) {
+    setValue = counterTry(setValue)
+    getClaim(claim);
+  };
+
+  let getNumber = function () {
+    let claim,
+      userNumber = prompt('Угадай число от 1 до 100');
+
+    if (userNumber === null) {
+      alert('Может, в следующий раз...');
+
+    } else if (!isNumber(userNumber) || (parseFloat(userNumber) > 100)) {
+      claim = confirm('Введи число от 0 до 100!');
+      getClaim(claim);
+
+    } else if (userNumber == numberForty) {
+      claim = confirm('Правильно! Сыграем еще?');
+
+      if (claim) {
+        numberForty = Math.floor(Math.random() * 101);
+        setValue = 10;
+        console.log("Загаданное число", numberForty);
+        game();
+      }
+
+    } else if (setValue > 1) {
+      if (userNumber < numberForty) {
+        claim = confirm('Неизвестное число больше, осталось попыток ' + counterTry(setValue));
+
+        incorrectAnswer(claim);
+
+      } else if (userNumber > numberForty) {
+        claim = confirm('Неизвестное число меньше, осталось попыток ' + counterTry(setValue));
+        incorrectAnswer(claim);
+      }
+
+    } else if (setValue === 1) {
+      claim = confirm('Попытки закончились, хотите сыграть еще?');
+
+      if (claim) {
+        numberForty = Math.floor(Math.random() * 101);
+        setValue = 10
+        console.log("Неизвестное число", numberForty);
+        number();
+      }
     }
   };
-  return getNumber;
+  return getNumber
 };
 
-let number = startGame();
+let number = createGame();
+
 number();
